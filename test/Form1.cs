b8b17177c9efcb8ad6153dcd51a58dbc9e8c90db
Anhuiqdb <1228520957@qq.com>
2018-12-25 16:44:36 +0800

    提交文件

---
+++ test/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string filePath = "";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //创建文件弹出选择窗口（包括文件名）对象
+            OpenFileDialog ofd = new OpenFileDialog();
+            //判断选择的路径
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                this.textBox1.Text = ofd.FileName.ToString();
+            }
+            filePath = this.textBox1.Text;
+        }
+    }
+}
