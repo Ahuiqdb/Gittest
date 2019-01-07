814335175763bc1a45825db2a4013b345ada09e1
Anhuiqdb <1228520957@qq.com>
2019-01-07 14:47:18 +0800

    字符串转html

---
+++ test/htmltoword.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class htmltoword : Form
+    {
+        public htmltoword()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+           string path = "d://wefwefw.html";
+            using (FileStream fs = File.Create(path))
+            {
+                byte[] info = new UTF8Encoding(true).GetBytes(this.richTextBox1.Text);
+                fs.Write(info, 0, info.Length);
+            }
+
+        }
+    }
+}
